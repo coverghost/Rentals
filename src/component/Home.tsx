2a1e5e9248815b350074d62bc41d76a73999909a
Ayush arya <ayush@brightdigitalgold.com>
2023-05-19 01:41:16 +0530

    success full token send

--- src/component/Home.tsx
+++ src/component/Home.tsx
@@ -1,10 +1,19 @@
 import React, { useEffect, useState } from "react";
 import "./css/home.css";
+import image from "./image/homebg.jpg";
 
 const Home = () => {
   return (
     <>
+      <div className="runing-news">
+        <p className="news">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iure enim
+          veritatis aperiam quaerat, cum harum unde eveniet maiores.
+        </p>
+      </div>
+
       <div className="home-body">
+        <img src={image} alt="rupeesimage" className="image-home" />
       </div>
     </>
   );
--- src/component/login.tsx
+++ src/component/login.tsx
@@ -1,62 +1,76 @@
-import React, { useState } from "react";
-import "./css/login.css";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import api from "../services/api";
 
 function LoginForm() {
-  const [mobile, setmobile] = useState("");
+  const [token, setToken] = useState("");
+  const [mobile, setMobile] = useState("");
   const [password, setPassword] = useState("");
-  const [L_mobile, setL_mobile] = useState("");
-  const [L_password, setL_Password] = useState("");
-  const [rePassword, setrePassword] = useState("");
-  const [name, setname] = useState("");
-
-  const [isLogin, setIsLogin] = useState(true);
   const [error, setError] = useState("");
+  const [isLogin, setIsLogin] = useState(true);
 
-  const handleL_PhoneChange = (event: any) => {
-    setL_mobile(event.target.value);
-  };
-  const handleL_PasswordChange = (event: any) => {
-    setL_Password(event.target.value);
-  };
+  useEffect(() => {
+    // Check if a token exists in local storage or cookie
+    const storedToken = localStorage.getItem("token");
 
-  const handlePhoneChange = (event: any) => {
-    setmobile(event.target.value);
-    if (event.target.value.length !== 10) {
-      setError("Enter Valid mobile number");
-    } else {
-      setError("");
+    if (storedToken) {
+      // Set the token in the state
+      setToken(storedToken);
     }
+  }, []);
+
+  const handlePhoneChange = (event:any) => {
+    setMobile(event.target.value);
   };
 
-  const handlePasswordChange = (event: any) => {
+  const handlePasswordChange = (event:any) => {
     setPassword(event.target.value);
   };
 
-  const handleConfirmPasswordChange = (event: any) => {
-    // setrePassword(event.target.value);
-    const { value } = event.target;
-    setrePassword(value);
-
-    if (value !== password) {
-      setError("Passwords do not match");
-    } else {
-      setError("");
+  const handleLogin = async () => {
+    try {
+      const response = await axios.post(api+"/login", {
+        mobile,
+        password,
+      });
+      const { token } = response.data;
+      setToken(token);
+      localStorage.setItem("token", token);
+    } catch (error) {
+      console.error(error);
+      setError("Invalid mobile or password");
     }
   };
 
-  const handleNameChange = (event: any) => {
-    setname(event.target.value);
+  const handleLogout = () => {
+    setToken("");
+    localStorage.removeItem("token");
   };
 
-  const handleSubmit = (event: any) => {
-    event.preventDefault();
+  const handleSignup = async () => {
+    try {
+      const response = await axios.post("/api/signup", {
+        mobile,
+        password,
+      });
+      const { token } = response.data;
+      setToken(token);
+      localStorage.setItem("token", token);
+    } catch (error) {
+      console.error(error);
+      setError("Signup failed");
+    }
+  };
 
-    console.log(
-      `Phone: ${mobile}, Password: ${password}, consfirm pass:${rePassword}, Name:${name}, isLogin: ${isLogin}`
-    );
-    console.log(
-      `Phone: ${L_mobile}, Password: ${L_password}, isLogin: ${isLogin}`
-    );
+  const handleSubmit = (event:any) => {
+    event.preventDefault();
+    setError("");
+
+    if (isLogin) {
+      handleLogin();
+    } else {
+      handleSignup();
+    }
   };
 
   const handleSwitchForm = () => {
@@ -64,107 +78,48 @@ function LoginForm() {
   };
 
   return (
-    <>
-      <div className="form_wraper">
+    <div className="form-wrapper">
+      {token ? (
+        <div>
+          <p>You are logged in</p>
+          <button onClick={handleLogout}>Logout</button>
+        </div>
+      ) : (
         <div className="form-container">
-          <h2 className="heading">{isLogin ? "Login" : "Sign Up"}</h2>
-          {isLogin ? (
-            <form onSubmit={handleSubmit} className="form-group">
-              <div className="form-group1">
-                <input
-                  type="phone"
-                  id="phone"
-                  value={L_mobile}
-                  onChange={handleL_PhoneChange}
-                  required
-                  placeholder="Phone"
-                />
-                {error && <p className="error_msg">{error}</p>}
-              </div>
-              <div className="form-group1">
-                <input
-                  type="password"
-                  id="password"
-                  value={L_password}
-                  onChange={handleL_PasswordChange}
-                  required
-                  placeholder="Password"
-                />
-              </div>
-              <button className="login_signup_button" type="submit">
-                {isLogin ? "Login" : "Sign Up"}
-              </button>
-            </form>
-          ) : (
-            <form onSubmit={handleSubmit} className="form-group">
-              <div className="form-group1">
-                <input
-                  type="text"
-                  id="name"
-                  value={name}
-                  onChange={handleNameChange}
-                  required
-                  placeholder="Name"
-                />
-              </div>
-              <div className="form-group1">
-                <input
-                  type="text"
-                  id="phone"
-                  value={mobile}
-                  onChange={handlePhoneChange}
-                  required
-                  placeholder="Phone"
-                />
-                {error && (
-                  <p className="error_msg">
-                    {error === "Enter Valid mobile number" ? error : ""}
-                  </p>
-                )}
-              </div>
-              <div className="form-group1">
-                <input
-                  type="password"
-                  id="password"
-                  value={password}
-                  onChange={handlePasswordChange}
-                  required
-                  placeholder="Password"
-                />
-              </div>
-              <div className="form-group1">
-                <input
-                  type="password"
-                  id="C_password"
-                  value={rePassword}
-                  onChange={handleConfirmPasswordChange}
-                  required
-                  placeholder="Confirm Password"
-                />
-                {error && (
-                  <p className="error_msg">
-                    {error === "Passwords do not match" ? error : ""}
-                  </p>
-                )}
-              </div>
-              <button className="login_signup_button" type="submit">
-                {isLogin ? "Login" : "Sign Up"}
-              </button>
-            </form>
-          )}
+          <h2>{isLogin ? "Login" : "Signup"}</h2>
+          <form onSubmit={handleSubmit}>
+            <div>
+              <label htmlFor="mobile">Mobile:</label>
+              <input
+                type="text"
+                id="mobile"
+                value={mobile}
+                onChange={handlePhoneChange}
+                required
+              />
+            </div>
+            <div>
+              <label htmlFor="password">Password:</label>
+              <input
+                type="password"
+                id="password"
+                value={password}
+                onChange={handlePasswordChange}
+                required
+              />
+            </div>
+            {error && <p>{error}</p>}
+            <button type="submit">{isLogin ? "Login" : "Signup"}</button>
+          </form>
           <p>
-            {isLogin ? "New to our site?" : "Already have an account? "}{" "}
-            <button
-              className="option_button "
-              type="button"
-              onClick={handleSwitchForm}
-            >
-              {isLogin ? "Sign-Up" : "Login"}
+            {isLogin ? "Don't have an account?" : "Already have an account?"}{" "}
+            <button onClick={handleSwitchForm}>
+              {isLogin ? "Signup" : "Login"}
             </button>
           </p>
         </div>
-      </div>
-    </>
+      )}
+    </div>
   );
 }
 
