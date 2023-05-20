009c46d71b4d0fbf5bb97a0a6d4f29e47e9eb93c
Ayush arya <ayush@brightdigitalgold.com>
2023-05-21 02:14:32 +0530

    login logout succesfully work

--- src/component/login.tsx
+++ src/component/login.tsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect, useContext } from "react";
 import axios from "axios";
 import api from "../services/api";
 import { MyContext } from "../context/Context";
-import './css/login.css'
+import "./css/login.css";
 import { useNavigate } from "react-router-dom";
 
 function LoginForm() {
@@ -10,13 +10,10 @@ function LoginForm() {
   const { setClosepopup } = useContext(MyContext);
   const { setUnitoken } = useContext(MyContext);
 
-
-
   const [token, setToken] = useState("");
   const [error, setError] = useState("");
   const [success, setSuccess] = useState("");
   const navigate = useNavigate();
-
 
   const [isLogin, setIsLogin] = useState(true);
   //login form
@@ -49,6 +46,11 @@ function LoginForm() {
     setPassword(event.target.value);
   };
 
+  const clearloginform = () => {
+    setMobile("");
+    setPassword("");
+  };
+
   // for sign up form
   const handleNameChange = (event: any) => {
     setname(event.target.value);
@@ -63,11 +65,18 @@ function LoginForm() {
     setS_C_Password(event.target.value);
   };
 
+  const clearsignupform = () => {
+    setS_Mobile("");
+    setS_Password("");
+    setname("")
+    setS_C_Password("")
+  };
+
   // Login function
   const handleLogin = async () => {
-    if(mobile.length<10){
-      console.log("invalidmobile number ")
-      return
+    if (mobile.length < 10) {
+      console.log("invalidmobile number ");
+      return;
     }
 
     try {
@@ -78,61 +87,64 @@ function LoginForm() {
       const { token } = response.data;
       setToken(token);
       localStorage.setItem("token", token);
-      setUnitoken(token)
+      setUnitoken(token);
       setLoggedIn(true); // Update login state
-      navigate("/dashboard")
-    } catch (error:any) {
+      navigate("/dashboard");
+    } catch (error: any) {
       setError(error.response.data.message);
+      clearloginform()
     }
   };
 
   // Signup function
   const handleSignup = async () => {
-    if(S_mobile.length<10 || (S_password !== S_C_password) || (S_password.length<6)){
-      return
+    if (
+      S_mobile.length < 10 ||
+      S_password !== S_C_password ||
+      S_password.length < 6
+    ) {
+      return;
     }
     try {
-      const response = await axios.post(api+"/user-register", {
+      const response = await axios.post(api + "/user-register", {
         name,
         S_mobile,
         S_password,
       });
-      const  data  = response.data;
+      const data = response.data;
       setSuccess(data.message);
-
-    } catch (error:any) {
+      clearsignupform()
+    } catch (error: any) {
       setError(error.response.data.message);
     }
   };
 
-
-
   const handleSubmit = (event: any) => {
     event.preventDefault();
-    if(S_mobile.length<10 ){
-      setError("Enter valid mobile Number")
-      return
+    if (S_mobile.length < 10) {
+      setError("Enter valid mobile Number");
+      return;
+    } else if (S_password.length < 6) {
+      setError("Password have atlest 6 character");
+      return;
     }
-    else if(S_password.length<6){
-      setError("Password have atlest 6 character")
-      return
-    }
-    if(S_password !== S_C_password){
-      setError("Password not match")
-      return
+    if (S_password !== S_C_password) {
+      setError("Password not match");
+      return;
     }
     setError("");
-
-    
   };
 
   const handleSwitchForm = () => {
+    setError("");
     setIsLogin(!isLogin);
   };
 
   return (
     <div className="form-wrapper">
-      {token ? setClosepopup(true) : (
+      {token ? (
+        setClosepopup(true)
+      ) : (
         <div className="form-container">
           <h2>{isLogin ? "Login" : "Signup"}</h2>
           {isLogin ? (
@@ -157,14 +169,18 @@ function LoginForm() {
                   placeholder="Password"
                 />
               </div>
-              {(error && <p className="error-message">{error}</p>)}
-              <button onClick={handleLogin} type="submit" className="submit-button">
+              {error && <p className="error-message">{error}</p>}
+              <button
+                onClick={handleLogin}
+                type="submit"
+                className="submit-button"
+              >
                 {isLogin ? "Login" : "Signup"}
               </button>
             </form>
           ) : (
             <form onSubmit={handleSubmit} className="formm">
-               <div>
+              <div>
                 <input
                   type="text"
                   id="name"
@@ -204,8 +220,14 @@ function LoginForm() {
                   placeholder="Confirm Password"
                 />
               </div>
-              {error?(error && <p className="error-message">{error}</p>):(success && <p className="success-message">{success}</p>)}
-              <button onClick={handleSignup} type="submit" className="submit-button">
+              {error
+                ? error && <p className="error-message">{error}</p>
+                : success && <p className="success-message">{success}</p>}
+              <button
+                onClick={handleSignup}
+                type="submit"
+                className="submit-button"
+              >
                 {isLogin ? "Login" : "Signup"}
               </button>
             </form>
