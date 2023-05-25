d2f17f19a61967a0598b9e76839d0e2ff6a28573
Ayush arya <ayush@brightdigitalgold.com>
2023-05-25 15:57:52 +0530

    home page

--- src/component/Home.tsx
+++ src/component/Home.tsx
@@ -1,19 +1,13 @@
 import React, { useEffect, useState } from "react";
 import "./css/home.css";
+import homeimage from "./image/home-image.png"
 
 const Home = () => {
   return (
     <>
-      <div className="container">
-        <div className="card">
-          <h2>Card Title</h2>
-          <p>Card content goes here.- right</p>
-        </div>
-        <div className="content">
-          {/* Add your existing content here */}
-          <div className="home-body">
-            {/* <img src={image} alt="rupeesimage" className="image-home" /> */}
-          </div>
+      <div className="home-body-conatainer">
+        <div className="home-body-card">
+         <img src={homeimage} alt="bankimage" className="home-body-content"/>
         </div>
       </div>
     </>
