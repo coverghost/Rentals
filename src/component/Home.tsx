d96f4b3bac52557a63145b79680b4c87de5a1279
Ayush arya <ayush@brightdigitalgold.com>
2023-05-25 16:21:48 +0530

    home page

--- src/component/Home.tsx
+++ src/component/Home.tsx
@@ -1,13 +1,27 @@
 import React, { useEffect, useState } from "react";
 import "./css/home.css";
-import homeimage from "./image/home-image.png"
+import homeimage from "./image/home-image.png";
 
 const Home = () => {
   return (
     <>
       <div className="home-body-conatainer">
         <div className="home-body-card">
-         <img src={homeimage} alt="bankimage" className="home-body-content"/>
+          <img src={homeimage} alt="bankimage" className="home-body-content" />
+        </div>
+        <div className="home-content">
+          <div>
+            <h1>Online Banking</h1>
+            <h4>The Power and Convenience of Online Banking</h4>
+            <p>
+              Online banking has transformed the way we handle our finances. It
+              offers convenience, allowing us to access our accounts, make
+              transactions, and pay bills from anywhere. With robust security
+              measures in place, online banking ensures our financial
+              information is safe. Embrace the ease and efficiency of online
+              banking today.
+            </p>
+          </div>
         </div>
       </div>
     </>
