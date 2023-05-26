2a9c55306c6f01c2894cb0ea4e4300c95538d796
Ayush arya <ayush@brightdigitalgold.com>
2023-05-26 17:29:08 +0530

    newdashboard_page

--- src/component/Home.tsx
+++ src/component/Home.tsx
@@ -6,6 +6,7 @@ const Home = () => {
   return (
     <>
       <div className="home-body-conatainer">
+        
         <div className="home-body-card">
           <img src={homeimage} alt="bankimage" className="home-body-content" />
         </div>
